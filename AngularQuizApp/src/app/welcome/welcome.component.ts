feb57ffb7c03eb48cb201ff57408d655cf589e01
Tirtharaj Das <tirtharajdas0@gmail.com>
2022-07-27 02:13:56 +0530

    first commit

---
+++ AngularQuizApp/src/app/welcome/welcome.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.scss']
+})
+export class WelcomeComponent implements OnInit {
+@ViewChild('name') namekey!:ElementRef;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+startQuiz(){
+  localStorage.setItem("name", this.namekey.nativeElement.value);
+}
+}
